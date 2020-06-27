9f615e2e50219c3015b6c8af18ffd48ea7e2df92
Anthony Lewis Knutson <knuts661@umn.edu>
2020-06-26 22:52:31 -0500

    print output to illustrate example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 #include <iostream>
 #include <vector>
@@ -12,18 +10,31 @@ std::vector< std::map<long int,int> > gid2pid;
 int main()
 {
 
-   gid2pid.resize( 4 ); 
+   gid2pid.resize( 4 ); // 4 element types 
 
    int h,i;
-   int isize[4] = {12, 11, 2, 10}; // example: 12 nodes, 11 faces, 2 cells, 10 ghosts
+
+   // example: 12 nodes, 11 faces, 2 cells, 10 ghosts
+   int isize[4] = {12, 11, 2, 10};
 
    for (h=0; h<4; ++h) { // loop through element types (h = handle)
       for (i=0; i<isize[ h ]; ++i) {
-         long int gid = 1000000000 + i; // doesn't matter, just has to be unique
-         int pid = i + 1; // starts at 1
+         long int gid = 10000000010 + 2345678900*(i+1);
+         int pid = i + 1; // start at 1
          gid2pid[ h ].insert(std::pair<long int,int>(gid,pid));
       }
    }
+
+   // print out face GID->PID map
+   std::map<long int,int> faces = gid2pid[ 1 ];
+   std::cout << "faces GID->PID\n";
+   std::map<long int,int>::iterator it = faces.begin();
+   while (it != faces.end())
+   {
+      std::cout << it->first << " | " << it->second << "\n";
+      it++;
+   }
+   faces.clear();
 
 #ifdef _DEBUG_
    std::cout << "\n";
@@ -31,17 +42,14 @@ int main()
       std::map<long int,int>::iterator it = gid2pid[ h ].begin();
       while (it != gid2pid[ h ].end())
       {
-         std::cout << it->first << " " << it->second << "\n";
+         std::cout << it->first << " | " << it->second << "\n";
          it++;
       }
       std::cout << "\n";
    }
-
 #endif
 
-   // does this take significantly more time or memory
-   // than indexing directly i.e. gid2pid[ 1 ]?
-   //std::map<long int,int> faces = gid2pid[ 1 ];
+   gid2pid.clear();
 
    return 0;
 }
